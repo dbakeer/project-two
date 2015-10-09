91520dc49bc4146a3509bc306988d6596420a824
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-10-09 11:45:53 -0400

    dependencies installed

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-var express  = require('express');
-    PORT     = process.env.PORT || 5432,
-    server   = express(),
-    MONGOURI = process.env.MONGOLAB_URI || "mongodb://localhot:27017",
-    dbname   = "CHANGE_THIS",
-    mongoose = require('mongoose');
+///////////////////////////////////////////////////////
+////////////////// DEPENDENCIES //////////////////////
+/////////////////////////////////////////////////////
 
-server.get('/test', function (req, res){
-  res.write("Welcome to my amazing app!");
+var express        = require('express');
+    PORT           = process.env.PORT || 5432,
+    server         = express(),
+    MONGOURI       = process.env.MONGOLAB_URI || "mongodb://localhot:27017",
+    dbname         = "CHANGE_THIS",
+    mongoose       = require('mongoose'),
+    morgan         = require('morgan'),
+    session        = require('session'),
+    methodOverride = require('method-override'),
+    bodyParser     = require('body-parser'),
+    nodemon        = require('nodemon'),
+    ejs            = require('ejs'),
+    layouts        = require('express-ejs-layouts'),
+    marked         = require('marked'),
+    bCrypt         = require('bcrypt'),
+    mongodb        = require('mongodb');
+
+
+///////////////////////////////////////////////////////
+///////////////////// BASE PAGE //////////////////////
+/////////////////////////////////////////////////////
+server.get('/', function (req, res) {
+  res.write("THIS IS THE FRONT PAGE!");
   res.end();
 });
 
 mongoose.connect(MONGOURI + "/" + dbname);
-server.listen(PORT, function (){
+server.listen(PORT, function () {
   console.log("SERVER IS UP ON PORT:", PORT);
 });
