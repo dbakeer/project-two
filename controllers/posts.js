16bec27e2d2a25f4eabd4b08239adae87b4fb6b6
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-10-12 22:00:22 -0400

    I made the thing work in the post controller yaaaas

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     router  = express.Router(),
-    Posts   = require('../models/posts.js'),
-    Users   = require('../models/user.js');
+    Post   = require('../models/posts.js'),
+    User   = require('../models/user.js');
 
 // Post-based Routes
 
@@ -9,8 +9,18 @@ router.get('/', function (req, res){
   res.render('posts/index');
 });
 
-router.post('/posts', function (req, res){
-
+router.post('/', function (req, res){
+  var posts = new Post(req.body.posts);
+  console.log(req.body);
+  posts.save(function (err, newPost){
+    if (err){
+      res.redirect(302, 'posts/new');
+      console.log("Post rejected");
+    } else {
+      console.log("Post saved!");
+      res.redirect(302, 'posts');
+    }
+  });
 });
 
 router.get('/new', function (req, res){
