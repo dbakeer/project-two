ca84ae24c7d58545936b44986a3ec93883d9bc45
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-10-15 19:31:22 -0400

    time to work from home

--- public/app.js
+++ public/app.js
@@ -1,20 +1,5 @@
 // UPVOTE DOWNVOTE TOGGLE
-$(function (){
-  $(".vote").click(
-    function(){
-    var count = parseInt($("~ .count", this).text());
 
-    if ($(this).hasClass("up")) {
-      var upvote = count + 1;
-
-      $("~ .count", this).text(upvote);
-    } else if
-      ($(this).hasClass("down")) {
-        var downvote = count - 1;
-        $("~ .count", this).text(downvote);
-      }
-  });
-});
 
 
 // LOGIN & SIGNUP MODALS
